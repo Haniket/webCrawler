73c794d28d64c5baa9ea657b2085b5cd85dee13f
Haniket <haniket2812001@gmail.com>
2023-01-27 12:29:53 +0530

    commit

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 const cors = require('cors');
 const express = require('express');
 require('dotenv').config();
-const crawlerService = require('./services/crawler.services');
+const crawlerService = require('./services/crawler.service');
 const app = express();
 app.use(cors());
 app.use(express.json({ extended: false }));
+const path = require('path')
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
+  index: ['index.html'],
+  maxAge: '1m',
+  redirect: false
+}
+app.use(express.static(path.join(__dirname,'client/dist'), options))
+
+// #############################################################################
+// Catch all handler for all other request.
+app.use('*', (req,res) => {
+  res.json({
+      at: new Date().toISOString(),
+      method: req.method,
+      hostname: req.hostname,
+      ip: req.ip,
+      query: req.query,
+      headers: req.headers,
+      cookies: req.cookies,
+      params: req.params
+    })
+    .end()
+})
+
+
+
 app.get('/', (req, res) => res.send('server is active'));
 app.get('/makeMap', crawlerService.getLinks);
 app.get('/resetMap', crawlerService.resetMap);
@@ -14,6 +43,9 @@ app.get('/getAllDomain', crawlerService.getAllDomain);
 // app.get("/makeMap",(req,res)=>{
 //   console.log(req);
 // })
-app.listen(3000, () => {
-  console.log('server is running on port 3000');
-});
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`)
+})
+
